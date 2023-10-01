b6f061c80123ca6f91cc65c33aafbb70760d4b8f
Isaac <isaacfungg@gmail.com>
2023-09-30 17:59:28 -0700

    init commit

--- account.js
+++ account.js
@@ -4,22 +4,18 @@ class Account {
       this.password = password;
     }
   
-    // Getter for username
     getUsername() {
       return this.username;
     }
-  
-    // Setter for username
+
     setUsername(username) {
       this.username = username;
     }
   
-    // Getter for password
     getPassword() {
       return this.password;
     }
   
-    // Setter for password
     setPassword(password) {
       this.password = password;
     }
